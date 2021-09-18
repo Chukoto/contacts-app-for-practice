f13a3f75c7609d72432c8165efa5f52613ea4ff1
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-18 12:02:51 +0900

    [mod] created mapActions to access to store actions and use directly

--- src/global/Header.vue
+++ src/global/Header.vue
@@ -4,7 +4,8 @@
       dark
       src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
     >
-      <v-app-bar-nav-icon @click="openSideMenu"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="openSideMenu">①</v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="openSideMenu">②</v-app-bar-nav-icon>
 
       <v-toolbar-title>マイアドレス帳</v-toolbar-title>
 
@@ -19,14 +20,22 @@
 </template>
 
 <script>
+// コンポーネントのメソッドに、storeのactionsメソッドを組み込む役割がある
+import { mapActions } from 'vuex';
+
 export default {
   name: 'Header',
   methods: {
+    // ①...mapActionsなし
     openSideMenu() {
       // コンポーネントのインスタンス上では、this.$storeでstoreにアクセスできる
       // dispatchメソッドで、storeのtoggleSideMenuアクションを呼び出している
       this.$store.dispatch('toggleSideMenu');
     },
+
+    // ②...mapActionsあり
+    // 分割代入でactionsをメソッドに組み込める
+    ...mapActions(['toggleSideMenu']),
   },
 };
 </script>
