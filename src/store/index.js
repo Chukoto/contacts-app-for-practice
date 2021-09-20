92ca6272cc3aeca7e3eb49d19b1e2ffb739aea28
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-20 16:20:18 +0900

    [mod] added google login method in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import firebase from 'firebase';
 
 Vue.use(Vuex);
 
@@ -17,6 +18,12 @@ export default new Vuex.Store({
     },
   },
   actions: {
+    login() {
+      // Google認証のプロバイダを利用する際に必要
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      // Googleの認証画面にリダイレクトし、ログイン後、ログインユーザーの情報を取得
+      firebase.auth().signInWithRedirect(google_auth_provider);
+    },
     // アクションメソッドの引数には自動的にcontextオブジェクトが渡る
     // ここではcontext.commitメソッドだけを受け取る
     toggleSideMenu({ commit }) {
