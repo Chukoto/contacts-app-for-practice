52b99b5aa1c423fbfc11dd7f240e1502504a0e06
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-20 16:21:50 +0900

    [mod] created login button and call  method for login

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,28 @@
 <template>
-  <v-container>
-    <div>ホーム</div>
+  <v-container text-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>マイアドレス帳</h1>
+        <p>
+          マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。
+        </p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color="info" @click="login">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions(['login']),
+  },
+};
 </script>
 
 <style></style>
